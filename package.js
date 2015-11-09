bb0754375541a9f10cb55106c8ab624f61cb0152
Uri Goldshtein <uri.goldshtein@gmail.com>
2015-11-09 12:06:52 +0200

    chore(dependency): Add Angular dependency and publish a new version

--- package.js
+++ package.js
@@ -1,13 +1,14 @@
 Package.describe({
   name: "dotansimha:accounts-ui-angular",
   summary: "AngularJS wrapper for Meteor's Account-UI package",
-  version: "0.0.1",
+  version: "0.0.2",
   documentation: 'README.md',
   git: "https://github.com/dotansimha/accounts-ui-angular"
 });
 
 Package.onUse(function (api) {
   api.versionsFrom('METEOR@1.2.0.1');
+  api.use('angular:angular@1.4.7', 'client');
   api.use('blaze-html-templates', 'client');
 
   api.imply('accounts-ui');
