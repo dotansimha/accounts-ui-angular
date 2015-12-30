2bf968255fe33db243b9a98428dd0b433dd4a1a5
peter.pavlovich <peter.pavlovich@enernoc.com>
2015-12-30 15:37:07 -0500

    revert unneeded change

--- login-buttons-directive.js
+++ login-buttons-directive.js
@@ -4,7 +4,7 @@ angular.module('accounts.ui').directive('loginButtons', function() {
     scope: true,
     template: '<div></div>',
     link: function(scope, element, attrs) {
-      Blaze.renderWithData(Template.loginButtons, {align: (attrs.align || '')}, element[0].parentElement);
+      Blaze.renderWithData(Template.loginButtons, {align: (attrs.align || '')}, element[0]);
     }
   }
 });
