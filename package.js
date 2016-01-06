76d6a57962398f89f243975b8e38103ca7eb9bd2
peter.pavlovich <peter.pavlovich@enernoc.com>
2016-01-06 16:40:18 -0500

    Update package name and version info to reflect a different package until the original author merges in the changes.

--- package.js
+++ package.js
@@ -1,9 +1,9 @@
 Package.describe({
-  name: "dotansimha:accounts-ui-angular",
+  name: "pavlovich:accounts-ui-angular",
   summary: "AngularJS wrapper for Meteor's Account-UI package",
-  version: "0.0.2",
+  version: "0.0.1",
   documentation: 'README.md',
-  git: "https://github.com/dotansimha/accounts-ui-angular"
+  git: "https://github.com/pavlovich/accounts-ui-angular"
 });
 
 Package.onUse(function (api) {
