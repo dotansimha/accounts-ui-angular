e5ee49f3477dbdc25f33195cb3091a1f854d45fd
dotansimha <dotansimha@gmail.com>
2016-03-21 12:58:21 +0200

    Fixed the package loading

--- accounts-ui-angular.js
+++ accounts-ui-angular.js
@@ -1,8 +1,11 @@
 import {checkNpmVersions} from 'meteor/tmeasday:check-npm-versions';
 
+if (!window.angular) {
+  require("angular");
+}
+
 checkNpmVersions({
   'angular': '^1.2.27'
 });
 
-require("angular");
 angular.module('accounts.ui', []);
